908ec65782836b287fe8b7e2a456e7f998cab9f5
Ruben Gonzalez <ruben1dario1999@gmail.com>
2021-06-23 11:36:23 -0430

    Updates

---
+++ precache-manifest.9b168ede4da4b6665869110b4024f21f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "86da5717ef621db6ab3a27df1f498090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72d64ecca1003f93c5e3",
+    "url": "/static/css/2.ad76a385.chunk.css"
+  },
+  {
+    "revision": "324e66fcb7b8592cfbcb",
+    "url": "/static/css/main.108fe165.chunk.css"
+  },
+  {
+    "revision": "72d64ecca1003f93c5e3",
+    "url": "/static/js/2.3f88dc80.chunk.js"
+  },
+  {
+    "revision": "2182b49f32ec797f3b9c375cbe5c7fb3",
+    "url": "/static/js/2.3f88dc80.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "324e66fcb7b8592cfbcb",
+    "url": "/static/js/main.daaf258d.chunk.js"
+  },
+  {
+    "revision": "45d009624ee73ac83b54",
+    "url": "/static/js/runtime-main.cdd681a7.js"
+  }
+]);
